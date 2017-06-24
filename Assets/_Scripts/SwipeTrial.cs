33ff02fb2dfe2a62b29f7dd7bca8e6b7f4b4f949
Fiona Hang <fiona.hang@mail.mcgill.ca>
2017-06-24 17:00:47 -0400

    :blowfish: toggle button
    - swipping and spinning are in On Value Changed
    - doesn't work, the toggle button doesnt't change when clicked

--- Assets/_Scripts/SwipeTrial.cs
+++ Assets/_Scripts/SwipeTrial.cs
@@ -9,6 +9,8 @@ public class SwipeTrial : MonoBehaviour {
 	Vector2 startPos; 					//2D start position of where we clicked/touched
 	Plane objPlane;						//plain plane :)
 
+	private bool isSwipe;
+
 	//me
 	//Vector2 currPos;
 
@@ -16,52 +18,61 @@ public class SwipeTrial : MonoBehaviour {
 
 		//flat plane at the position of the swipe object and facing towards camera
 		objPlane = new Plane (Camera.main.transform.forward * -1, this.transform.position);
+
+		isSwipe = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		//when we touch the screen
-		if ((Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began) || Input.GetMouseButtonDown (0)) {
+		if ( isSwipe ) {
 
-			//the instance of the line is created
-			thisTrail = (GameObject)Instantiate (trailPrefab, this.transform.position, Quaternion.identity);
+			//when we touch the screen
+			if ((Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began) || Input.GetMouseButtonDown (0)) {
 
-			//Ray based on position of the mouse
-			Ray mRay = Camera.main.ScreenPointToRay (Input.mousePosition);
-			float rayDistance;
+				//the instance of the line is created
+				thisTrail = (GameObject)Instantiate (trailPrefab, this.transform.position, Quaternion.identity);
 
-			if (objPlane.Raycast (mRay, out rayDistance))
+				//Ray based on position of the mouse
+				Ray mRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+				float rayDistance;
+
+				if (objPlane.Raycast (mRay, out rayDistance))
 				//record swipe object position -> used in the 2nd else if
 				startPos = mRay.GetPoint (rayDistance);
-		}
-
-		//if there's been more than 1 touch in this screen and if first touch is moving, OR if left mouse button down
-		else if(((Input.touchCount>0 && Input.GetTouch(0).phase == TouchPhase.Moved) || Input.GetMouseButton(0))){
-
-			//Ray based on position of the mouse
-			Ray mRay = Camera.main.ScreenPointToRay (Input.mousePosition);
-			float rayDistance;
-
-			if (objPlane.Raycast (mRay, out rayDistance)) {
-				//update swipe object position
-				//moving the trail to that position
-				thisTrail.transform.position = mRay.GetPoint (rayDistance);
-				//print(mRay.GetPoint(rayDistance));
-
-				//currPos = mRay.GetPoint (rayDistance); 
-				//thisTrail.transform.position = currPos;
-
 			}
 
+			//if there's been more than 1 touch in this screen and if first touch is moving, OR if left mouse button down
+			else if (((Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Moved) || Input.GetMouseButton (0))) {
+
+					//Ray based on position of the mouse
+					Ray mRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+					float rayDistance;
+
+					if (objPlane.Raycast (mRay, out rayDistance)) {
+						//update swipe object position
+						//moving the trail to that position
+						thisTrail.transform.position = mRay.GetPoint (rayDistance);
+						//print(mRay.GetPoint(rayDistance));
+
+						//currPos = mRay.GetPoint (rayDistance); 
+						//thisTrail.transform.position = currPos;
+
+					}
+
+				}
+
+			//if distance between start position and final position is very small, destroy object (not a drawing)
+			else if ((Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Ended) || Input.GetMouseButtonUp (0)) {
+
+					if (Vector2.Distance (thisTrail.transform.position, startPos) < 0.1)								//2D
+					Destroy (thisTrail);
+
+			}
 		}
+	}
 
-		//if distance between start position and final position is very small, destroy object (not a drawing)
-		else if((Input.touchCount>0 && Input.GetTouch(0).phase == TouchPhase.Ended) || Input.GetMouseButtonUp(0)){
-
-			if(Vector2.Distance(thisTrail.transform.position, startPos) < 0.1)								//2D
-				Destroy(thisTrail);
-
-		}
+	public void Swiping(bool value){
+		isSwipe = !value;
 	}
 }
